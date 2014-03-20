5e2d6d3eacd8ce970389b3ce537acbb21d6cd877
nqounet <nqou.net@gmail.com>
2014-03-21 05:00:12 +0900

    ようやく動いた(master)

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,7 +199,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= config.app %>/images',
-                    src: '{,*/}*.{gif,jpeg,jpg,png}',
+                    src: '{,*/}sample.',
                     dest: '<%= config.dist %>/images'
                 }]
             }
