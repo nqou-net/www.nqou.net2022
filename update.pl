95be87aca3a7e33c1cc20fa8f0592366e37ac97f
nqounet <nqou.net@gmail.com>
2014-04-29 04:04:39 +0900

    デバッグ仕様になっていたのを修正(master)

--- update.pl
+++ update.pl
@@ -2,13 +2,10 @@
 use utf8;
 use 5.012;
 
-use Data::Printer;
 use JSON::XS qw(decode_json);
 use Path::Tiny qw(path);
 
 my $json = decode_json(path('package.json')->slurp);
 for my $key (sort keys $json->{devDependencies}) {
-    print qq{
-        system(qq{npm install -D $key});
-    };
+    system(qq{npm install -D $key});
 }
