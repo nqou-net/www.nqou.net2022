eac90586d689087fd4a50d30391943eeece90636
nqounet <mail@nqou.net>
2021-10-30 19:45:53 +0900

    update コマンドを追加(hugo)

--- tools/rename_posts.pl
+++ tools/rename_posts.pl
@@ -3,18 +3,18 @@ use utf8;
 use strict;
 use warnings;
 use Path::Tiny qw(path);
-use YAML         ();
+use YAML ();
 use Time::Moment ();
 
 my $root = path(__FILE__)->absolute->parent(2);
 
 # 記事を取得
 my $posts_dir = path($root, 'content', 'post');
-my $iterator = $posts_dir->iterator(+{recurse => 1});
+my $iterator = $posts_dir->iterator(+{ recurse => 1 });
 
 # タイムゾーンを取得
-my $tm        = Time::Moment->now;
-my $offset    = $tm->offset;
+my $tm = Time::Moment->now;
+my $offset = $tm->offset;
 my $jp_offset = 540;
 
 # 記事から時間を取得して、名称変更
@@ -24,6 +24,9 @@ while (my $post = $iterator->()) {
     my $info = YAML::Load($yaml);
 
     my $modified;
+    unless (exists $info->{draft}) {
+        $info->{draft} = 'true'
+    }
 
     # ISO8601を持っていない場合はdateからISOを作成
     unless (exists $info->{iso8601}) {
@@ -47,16 +50,16 @@ while (my $post = $iterator->()) {
 
         # date があれば iso8601 を作成
         else {
-            my $date     = join('T', split(/\s/, $info->{date})) . 'Z';
-            my $tm       = Time::Moment->from_string($date);
+            my $date = join('T', split(/\s/, $info->{date})) . 'Z';
+            my $tm = Time::Moment->from_string($date);
             my $new_date = $tm->with_offset_same_local($offset);
             $info->{iso8601} = $new_date->to_string;
         }
 
         # date はURLのために日本時間へ統一
-        my $tm       = Time::Moment->from_string($info->{iso8601});
+        my $tm = Time::Moment->from_string($info->{iso8601});
         my $url_date = $tm->with_offset_same_instant($jp_offset);
-        $info->{date} = $url_date->strftime('%F %T');
+        $info->{date} = $url_date->to_string;
         $modified = 1;
     }
 
@@ -88,16 +91,18 @@ while (my $post = $iterator->()) {
         $post->spew_utf8($body);
     }
 
-    # 時間でファイル名を変更
-    my $new_path = new_path_by_iso8601($info->{iso8601});
-    $new_path->parent->mkpath;
-    $post->move($new_path->stringify);
+    # 公開する場合は時間でファイル名を変更
+    if ($info->{draft} eq 'false') {
+        my $new_path = new_path_by_iso8601($info->{iso8601});
+        $new_path->parent->mkpath;
+        $post->move($new_path->stringify);
+    }
 }
 
 sub new_path_by_iso8601 {
-    my $iso8601  = shift;
-    my $tm       = Time::Moment->from_string($iso8601)->with_offset_same_instant($jp_offset);
-    my $path     = $tm->strftime('%Y/%m/%d/%H%M%S');
+    my $iso8601 = shift;
+    my $tm = Time::Moment->from_string($iso8601)->with_offset_same_instant($jp_offset);
+    my $path = $tm->strftime('%Y/%m/%d/%H%M%S');
     my $new_path = path($posts_dir, qq{$path.md})->absolute;
     return $new_path;
 }
